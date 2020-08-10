373fb1b154f47fd6f8762138331a442df2dd4efd
jonathanrchamorro <68171390+jonathanrchamorro@users.noreply.github.com>
2020-08-09 19:32:54 -0500

    Se crea el reto de la semana Numero 3

--- Python/conversor.py
+++ Python/conversor.py
@@ -1,7 +1,39 @@
-pesos = input ("Cuantos pesos colombianos tienes?: ")
-pesos = float (pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round (dolares, 2)
-dolares = str (dolares)
-print ("Tienes $" + dolares + " Dolares")
+#con command mas / comentamos bloques de codigo
+# con comillas """ podemos crear bloques de cadenas de textos"""
+menu = """ Bienvenido al conversor de monedas 🥇
+    1 - Pesos Colombianos
+    2 - Pesos Argentinos
+    3 - Pesos Mexicanos
+    
+    Elige una opcion: """
+
+opcion = int(input(menu))
+if opcion == 1:
+    pesos = input ("Cuantos pesos colombianos tienes?: ")
+    pesos = float (pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round (dolares, 2)
+    dolares = str (dolares)
+    print ("Tienes $" + dolares + " Dolares")
+elif opcion == 2:
+    pesos = input ("Cuantos pesos Argentinos tienes?: ")
+    pesos = float (pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round (dolares, 2)
+    dolares = str (dolares)
+    print ("Tienes $" + dolares + " Dolares")
+elif opcion == 3:
+    pesos = input ("Cuantos pesos Mexicanos tienes?: ")
+    pesos = float (pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round (dolares, 2)
+    dolares = str (dolares)
+    print ("Tienes $" + dolares + " Dolares")
+else:
+    print ("Elige una opcion valida por favor 😂 ")
+
+
+
